5619d101858cb4bcdc56e61b3cadb00d83271b6f
Eric <ericl343@nycstudents.net>
2024-09-10 17:36:27 -0400

    Lab 1

---
+++ Lin_Eric_Lab_1.cpp
@@ -0,0 +1,47 @@
+ //Lin_Eric
+ //Lab_1
+ //Sepetember_10_2024
+
+ #include <iostream>
+
+using namespace std;
+
+int main()
+//added the main code
+{
+   cout << "########################################################################################" << endl;
+   cout << "#                                                                                      #" << endl;
+   cout << "#                                                                                      #" << endl;
+   cout << "# Ways to access the Task Manager on your Windows computer:=                           #" << endl;
+   cout << "#                                                                                      #" << endl;
+   cout << "#           Press the key combination Ctrl + Shift + Escape =                          #" << endl;
+   cout << "#                                                                                      #" << endl;
+   cout << "#           Press the key combination Ctrl + Alt + Delete and select “Task Manager” =  #" << endl;
+   cout << "#                                                                                      #" << endl;
+   cout << "#           Type “Task Manager” in the Windows Start menu search =                     #" << endl;
+   cout << "#                                                                                      #" << endl;
+   cout << "#                                                                                      #" << endl;
+   cout << "########################################################################################" << endl;
+//added # to surrond
+
+   return 0;
+}
+
+/*[Running] cd "c:\Users\Eric.Lin23\Documents\GitHub\lab-1-cout-statements-ericl343\lab-1-cout-statements-ericl343\" && g++ Lin_Eric_Lab_1.cpp -o Lin_Eric_Lab_1 && "c:\Users\Eric.Lin23\Documents\GitHub\lab-1-cout-statements-ericl343\lab-1-cout-statements-ericl343\"Lin_Eric_Lab_1
+########################################################################################
+#                                                                                      #
+#                                                                                      #
+# Ways to access the Task Manager on your Windows computer:=                           #
+#                                                                                      #
+#           Press the key combination Ctrl + Shift + Escape =                          #
+#                                                                                      #
+#           Press the key combination Ctrl + Alt + Delete and select “Task Manager” =  #
+#                                                                                      #
+#           Type “Task Manager” in the Windows Start menu search =                     #
+#                                                                                      #
+#                                                                                      #
+########################################################################################
+
+[Done] exited with code=0 in 2.429 seconds*/
+
+//copy and pasted the output
